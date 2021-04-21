b7b01bdbec88b35e77d028eaddd35a0f6f3d258d
taylortreece <tayloratreece@gmail.com>
2021-04-20 23:17:05 -0500

    commit tree e00c864c7c40d0eb869164ea9e4ec80ed53bf180

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    if @post.valid?
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
+    @post.update(post_params) ? redirect_to(post_path(@post)) : render(:edit)
   end
 
   private
